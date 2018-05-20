79a3d5d00764180b44a867592b900031c94a6c5a
Richard L. Hunter <pur3miish@me.com>
2018-05-20 13:20:22 +1000

    Message types added.

---
+++ src/message-types.mjs
@@ -0,0 +1,26 @@
+/**
+ * The WebSocket Client and server communication messages.
+ * @namespace
+ * @property {string} GQL_CONNECTION_INIT  Client to server string to initialize connection.
+ * @property {string} GQL_CONNECTION_ACK  The server response indicationg the websocket connection has been acknowledged.
+ * @property {string} GQL_CONNECTION_ERROR A server connection error response.
+ * @property {string} GQL_CONNECTION_KEEP_ALIVE Hearbeat of the ws subscription. See {@link https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_servers#Pings_and_Pongs_The_Heartbeat_of_WebSockets The HeartBeat of WebSockets}
+ * @property {string} GQL_CONNECTION_TERMINATE Client request to terminate the WebSocket connection.
+ * @property {string} GQL_START Client request to start listening to a GraphQL subscription opperation.
+ * @property {string} GQL_DATA Server response with data.
+ * @property {string} GQL_ERROR Server GraphQL Error response.
+ * @property {string} GQL_COMPLETE Server response indicating GraphQL subscription opperation is complete.
+ * @property {string} GQL_STOP Client request stop listening to a subscription opperation.
+ */
+export const MessageTypes = {
+  GQL_CONNECTION_INIT: 'connection_init',
+  GQL_CONNECTION_ACK: 'connection_ack',
+  GQL_CONNECTION_ERROR: 'connection_error',
+  GQL_CONNECTION_KEEP_ALIVE: 'ka',
+  GQL_CONNECTION_TERMINATE: 'connection_terminate',
+  GQL_START: 'start',
+  GQL_DATA: 'data',
+  GQL_ERROR: 'error',
+  GQL_COMPLETE: 'complete',
+  GQL_STOP: 'stop'
+}
