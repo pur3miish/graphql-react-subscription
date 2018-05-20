ec26c76d129c5d828fd7f694ff44a2d269991f15
Richard L. Hunter <pur3miish@me.com>
2018-05-20 13:22:46 +1000

    Removed.

--- src/helpers.mjs
+++ src/helpers.mjs
@@ -1,13 +1,6 @@
 export const isString = value =>
   typeof value === 'string' || value instanceof String
 
-export const isObject = value =>
-  !!value && (typeof value == 'object' || typeof value == 'function')
-
-export const isFunc = value => typeof value == 'function'
-
-export const ssr = typeof window === 'undefined'
-
 export const _global =
   typeof global !== 'undefined'
     ? global
