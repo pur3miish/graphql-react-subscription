3d9400fe6a82087fab7f0cd66ab6b5536f905c1c
Richard L. Hunter <pur3miish@me.com>
2018-05-22 14:31:09 +1000

    Graphql-react-subscription setup with next and graphql-react.
    - Graphql resolvers, and typedefs added.
    - Koa server.
    - Next js.

---
+++ example/pages/index.js
@@ -0,0 +1,8 @@
+import { Fragment } from 'react'
+import provider from '../provider'
+
+export default provider(
+  <Fragment>
+    <h1>Example Next.js app &amp; GraphQL API</h1>
+  </Fragment>
+)
---
+++ example/provider.js
@@ -0,0 +1,23 @@
+import 'isomorphic-unfetch'
+import { GraphQL, Provider, preload } from 'graphql-react'
+import Head from 'next/head'
+import { GraphQLSocket } from 'graphql-react-subscription'
+
+let graphql
+
+export default children => {
+  const GraphQLProvider = ({ cache }) => {
+    graphql = (process.browser && graphql) || new GraphQL({ cache })
+    return <Provider value={graphql}>{children}</Provider>
+  }
+
+  if (!process.browser)
+    GraphQLProvider.getInitialProps = async () => {
+      const graphql = new GraphQL()
+      await preload(<Provider value={graphql}>{children}</Provider>)
+      Head.rewind()
+      return { cache: graphql.cache }
+    }
+
+  return GraphQLProvider
+}
